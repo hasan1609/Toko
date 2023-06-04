ef1fa5a6ec35e3cb4889d8a8143d76fd9cfa49db
hasanb1609 <hasanb1609@umsida.ac.id>
2023-06-04 23:34:40 +0700

    uploadproduk

--- app/src/main/java/com/go4sumbergedang/toko/ui/activity/AddProdukActivity.kt
+++ app/src/main/java/com/go4sumbergedang/toko/ui/activity/AddProdukActivity.kt
@@ -1,19 +1,17 @@
 package com.go4sumbergedang.toko.ui.activity
-
 import android.Manifest
-import android.app.Activity
 import android.app.ProgressDialog
 import android.content.ContentResolver
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
@@ -27,8 +25,6 @@ import okhttp3.MediaType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import org.jetbrains.anko.AnkoLogger
-import org.jetbrains.anko.info
-import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
 import retrofit2.Call
 import retrofit2.Callback
@@ -36,62 +32,77 @@ import retrofit2.Response
 import java.io.File
 import java.io.FileOutputStream
 
-class AddProdukActivity : AppCompatActivity(), AnkoLogger {
+class AddProdukActivity : AppCompatActivity(), AnkoLogger, BottomSheetFilePickerFragment.FilePickerListener {
 
-    lateinit var binding: ActivityAddProdukBinding
+    private lateinit var binding: ActivityAddProdukBinding
+    private lateinit var progressDialog: ProgressDialog
     var api = ApiClient.instance()
-    lateinit var progressDialog: ProgressDialog
-    private var selectedFile: Uri? = null
+    private var selectedImageFile: File? = null
 
-    companion object {
-        private const val REQUEST_IMAGE_PICKER = 100
-    }
+    private val PERMISSION_REQUEST_CODE = 100
+    private val REQUEST_IMAGE_CAPTURE = 1
+    private val REQUEST_IMAGE_PICKER = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_add_produk)
         binding.lifecycleOwner = this
         progressDialog = ProgressDialog(this)
-        setupToolbar()
 
         binding.addFoto.setOnClickListener {
-            checkPermissionsAndOpenImagePicker()
-        }
-        binding.btnSimpan.setOnClickListener {
-            uploadData()
-        }
-    }
-    private fun setupToolbar() {
-        setSupportActionBar(binding.toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setDisplayShowHomeEnabled(true)
-        supportActionBar?.setDisplayShowTitleEnabled(true)
-        supportActionBar?.setTitle("Tambah Produk")
-        binding.toolbar.setNavigationIcon(R.drawable.back)
-        binding.toolbar.setNavigationOnClickListener {
-            onBackPressed()
-        }
-    }
-
-    private fun checkPermissionsAndOpenImagePicker() {
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                REQUEST_IMAGE_PICKER
-            )
-        } else {
             openImagePicker()
+        }
+
+        binding.btnSimpan.setOnClickListener {
+            selectedImageFile?.let { file ->
+                uploadData(file)
+            }
         }
     }
 
     private fun openImagePicker() {
-        ImagePicker.with(this)
-            .start()
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.CAMERA
+                ),
+                PERMISSION_REQUEST_CODE
+            )
+        } else {
+            val bottomSheetFragment = BottomSheetFilePickerFragment()
+            bottomSheetFragment.show(supportFragmentManager, "filePicker")
+        }
+    }
+
+    override fun onFilePickerOptionSelected(option: Int) {
+        when (option) {
+            BottomSheetFilePickerFragment.OPTION_CAMERA -> {
+                openCamera()
+            }
+            BottomSheetFilePickerFragment.OPTION_GALLERY -> {
+                openGallery()
+            }
+        }
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, REQUEST_IMAGE_PICKER)
+    }
+
+    private fun openCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
     }
 
     override fun onRequestPermissionsResult(
@@ -100,27 +111,60 @@ class AddProdukActivity : AppCompatActivity(), AnkoLogger {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_IMAGE_PICKER) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 openImagePicker()
+            } else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
             }
         }
     }
-
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == ImagePicker.REQUEST_CODE) {
-                binding.foto.setImageURI(data?.data)
-                selectedFile = data?.data
+        if (resultCode == RESULT_OK) {
+            when (requestCode) {
+                REQUEST_IMAGE_PICKER -> {
+                    val imageUri = data?.data
+                    binding.foto.setImageURI(imageUri)
+                    selectedImageFile = File(getRealPathFromURI(imageUri))
+                }
+                REQUEST_IMAGE_CAPTURE -> {
+                    val imageBitmap = data?.extras?.get("data") as Bitmap
+                    binding.foto.setImageBitmap(imageBitmap)
+                    selectedImageFile = saveImageToInternalStorage(imageBitmap)
+                }
             }
         }
     }
 
-    private fun uploadData(){
-        val file = File(selectedFile?.path ?: "")
+    private fun getRealPathFromURI(uri: Uri?): String {
+        var realPath = ""
+        uri?.let {
+            val cursor = contentResolver.query(it, null, null, null, null)
+            cursor?.use {
+                if (it.moveToFirst()) {
+                    val idx = it.getColumnIndex(MediaStore.Images.ImageColumns.DATA)
+                    realPath = it.getString(idx)
+                }
+            }
+        }
+        return realPath
+    }
+
+    private fun saveImageToInternalStorage(image: Bitmap): File {
+        val fileDir = filesDir
+        val fileName = "image_${System.currentTimeMillis()}.jpg"
+        val file = File(fileDir, fileName)
+        FileOutputStream(file).use { outputStream ->
+            image.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+            outputStream.flush()
+        }
+        return file
+    }
+
+    private fun uploadData(file: File) {
         val namaProduk = binding.edtNama.text.toString()
         val hargaProduk = binding.edtHarga.text.toString()
         val keteranganProduk = binding.edtKeterangan.text.toString()
@@ -132,11 +176,16 @@ class AddProdukActivity : AppCompatActivity(), AnkoLogger {
         }
 
         if (namaProduk.isEmpty() || hargaProduk.isEmpty() || kategori.isEmpty()) {
-            toast("Jangan Kosongi kolom")
+            Toast.makeText(this, "Jangan kosongi kolom", Toast.LENGTH_SHORT).show()
             return
         }
+
         val requestBody = RequestBody.create(MediaType.parse("image/*"), file)
-        val imagePart : MultipartBody.Part = MultipartBody.Part.createFormData("foto_makanan", file.name, requestBody)
+        val imagePart: MultipartBody.Part = MultipartBody.Part.createFormData(
+            "foto_makanan",
+            file.name,
+            requestBody
+        )
         val requestrestoId: RequestBody = RequestBody.create(
             MediaType.parse("text/plain"),
             "c9927b1a-a334-4bd8-9633-62fb9b843b85"
@@ -160,35 +209,29 @@ class AddProdukActivity : AppCompatActivity(), AnkoLogger {
                 call: Call<ResponsePostData>,
                 response: Response<ResponsePostData>
             ) {
-                try {
-                    if (response.isSuccessful) {
-                        loading(false)
-                        finish()
-                    } else {
-                        loading(false)
-                        toast("Gagal menambahkan produk")
-                    }
-                } catch (e: Exception) {
+                if (response.isSuccessful) {
                     loading(false)
-                    info { "hasan ${e.message}" }
-                    toast(e.message.toString())
+                    finish()
+                } else {
+                    loading(false)
+                    toast("Gagal menambahkan produk")
                 }
             }
 
             override fun onFailure(call: Call<ResponsePostData>, t: Throwable) {
                 loading(false)
-                info { "hasan ${t.message}" }
-                toast(t.message.toString())
+                toast("Terjadi kesalahan")
+                Log.e("AddProdukActivity", "Error: ${t.localizedMessage}")
             }
         })
     }
 
-    fun loading(status : Boolean){
-        if (status){
-            progressDialog.setTitle("Loading...")
-            progressDialog.setCanceledOnTouchOutside(false)
+    private fun loading(isLoading: Boolean) {
+        if (isLoading) {
+            progressDialog.setMessage("Tunggu sebentar...")
+            progressDialog.setCancelable(false)
             progressDialog.show()
-        }else{
+        } else {
             progressDialog.dismiss()
         }
     }
--- app/src/main/java/com/go4sumbergedang/toko/ui/fragment/BottomSheetFilePickerFragment.kt
+++ app/src/main/java/com/go4sumbergedang/toko/ui/fragment/BottomSheetFilePickerFragment.kt
@@ -1,98 +1,66 @@
 package com.go4sumbergedang.toko.ui.fragment
 
-import android.Manifest
-import android.app.Activity
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.ImageDecoder
-import android.os.Build
+import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.go4sumbergedang.toko.R
 import com.go4sumbergedang.toko.databinding.BottomSheetFilePickBinding
-import com.go4sumbergedang.toko.ui.activity.AddProdukActivity
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import org.jetbrains.anko.AnkoLogger
-import java.io.IOException
 
-class BottomSheetFilePickerFragment : BottomSheetDialogFragment(), AnkoLogger {
-    lateinit var binding: BottomSheetFilePickBinding
-    private var onImageSelectedListener: ((Bitmap) -> Unit)? = null
+class BottomSheetFilePickerFragment : BottomSheetDialogFragment() {
+
+    private var listener: FilePickerListener? = null
+
+    private var _binding: BottomSheetFilePickBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = DataBindingUtil.inflate(inflater,R.layout.bottom_sheet_file_pick,container,false)
-        binding.lifecycleOwner = this
-        binding.iconKamera.setOnClickListener {
-            openCamera()
-        }
-
-        binding.iconFile.setOnClickListener {
-            openGallery()
-        }
+    ): View {
+        _binding = BottomSheetFilePickBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    private fun openCamera() {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivityForResult(intent, CAMERA_REQUEST_CODE)
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    private fun openGallery() {
-        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        intent.type = "image/*"
-        startActivityForResult(intent, GALLERY_REQUEST_CODE)
-    }
+        binding.iconKamera.setOnClickListener {
+            listener?.onFilePickerOptionSelected(OPTION_CAMERA)
+            dismiss()
+        }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK) {
-            when (requestCode) {
-                CAMERA_REQUEST_CODE -> {
-                    val imageBitmap = data?.extras?.get("data") as? Bitmap
-                    imageBitmap?.let { onImageSelectedListener?.invoke(it) }
-                }
-                GALLERY_REQUEST_CODE -> {
-                    val imageUri = data?.data
-                    imageUri?.let {
-                        try {
-                            val bitmap = if (Build.VERSION.SDK_INT < 28) {
-                                MediaStore.Images.Media.getBitmap(
-                                    requireActivity().contentResolver,
-                                    imageUri
-                                )
-                            } else {
-                                val source =
-                                    ImageDecoder.createSource(requireActivity().contentResolver, imageUri)
-                                ImageDecoder.decodeBitmap(source)
-                            }
-                            onImageSelectedListener?.invoke(bitmap)
-                        } catch (e: IOException) {
-                            e.printStackTrace()
-                        }
-                    }
-                }
-            }
+        binding.iconFile.setOnClickListener {
+            listener?.onFilePickerOptionSelected(OPTION_GALLERY)
             dismiss()
         }
     }
 
-    fun setOnImageSelectedListener(listener: (Bitmap) -> Unit) {
-        onImageSelectedListener = listener
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is FilePickerListener) {
+            listener = context
+        } else {
+            throw RuntimeException("$context must implement FilePickerListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    interface FilePickerListener {
+        fun onFilePickerOptionSelected(option: Int)
     }
 
     companion object {
-        private const val CAMERA_REQUEST_CODE = 1
-        private const val GALLERY_REQUEST_CODE = 2
-        private const val CAMERA_PERMISSION_REQUEST_CODE = 3
+        const val OPTION_CAMERA = 0
+        const val OPTION_GALLERY = 1
     }
 }
